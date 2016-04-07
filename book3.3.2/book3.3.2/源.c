7615bc9c18083af808e4c0ffa6a0346aa028eaf7
Haoning <wuhaoning0503@hotmail.com>
2016-04-07 17:42:24 +0800

    book3.3.2
    Example 3.3.2 on C homework's books

---
+++ book3.3.2/book3.3.2/源.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main()
+{
+	double m, cost1, cost2, cost3;
+	printf("你的行李质量是多少千克？\n");
+	scanf_s("%lf", &m);
+	if (m <= 50)
+	{
+		cost1 = 0.25 * m;
+		printf("运费是：%f 元\n", cost1);
+	}
+	else if (m <= 100)//50 < m <=100
+	{
+		cost2 = 0.35*(m - 50) + 0.25 * 50;
+		printf("运费是：%f 元\n", cost2);
+	}
+	else if (m > 100)//50 < m, 100 < m
+	{
+		cost3 = 0.45*(m - 100) + 50 * 0.25 + 50 * 0.35;
+		printf("运费是：%f 元\n", cost3);
+	}
+	system("pause");
+}
