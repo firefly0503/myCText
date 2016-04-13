ae3dcb8b802b965afcb815f05bdeb631c9fa7f28
Haoning <wuhaoning0503@hotmail.com>
2016-04-13 16:12:05 +0800

    update
    update and fix bugs

--- book3.3.2/book3.3.2/源.c
+++ book3.3.2/book3.3.2/源.c
@@ -5,21 +5,22 @@ void main()
   {	
 	double m, cost1, cost2, cost3;
 	printf("你的行李质量是多少千克？\n");
-	scanf_s("%f", &m);
-	if (m <= 50)
-	{
-		cost1 = 0.25 * m;
-		printf("运费是：%f 元\n", cost1);
-	}
-	    else if (m <= 100)//50 < m <=100
+	scanf_s("%lf", &m);
+	if (m >= 0);
+	    if (m <= 50)
 	    {
-		    cost2 = 0.35*(m - 50) + 0.25 * 50;
-		    printf("运费是：%f 元\n", cost2);
+		    cost1 = 0.25 * m;
+		    printf("运费是：%lf 元\n", cost1);
 	    }
-	         else if (m > 100)//50 < m, 100 < m
+	    else if (m <= 100)
 	         {
-	          	 cost3 = 0.45*(m - 100) + 50 * 0.25 + 50 * 0.35;
-		         printf("运费是：%f 元\n", cost3);
+		         cost2 = 0.35*(m - 50) + 0.25 * 50;
+		         printf("运费是：%lf 元\n", cost2);
 	         }
+	         else if (m > 100)
+	              {
+	          	      cost3 = 0.45*(m - 100) + 50 * 0.25 + 50 * 0.35;
+		              printf("运费是：%lf 元\n", cost3);
+	              }
   }
 }
